be5859a1f68b8ad88bca8f6f4f57bd25f712b989
homesoft <vnhomesoft@gmail.com>
2023-09-01 23:37:48 +0700

    Add project files.

---
+++ CodeConventionDemo/Shop.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeConventionDemo
+{
+	public class Shop
+	{
+        private string _name; // try rename remove underscore prefix and rebuild to see error
+        public string name
+        {
+            get { return _name; }
+            set { _name = value; }
+        }
+
+		public Shop() {
+			_name = "Tom";
+		}
+	}
+}
